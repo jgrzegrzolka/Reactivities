1e516fe2ba8ebc50790ca8cd562d065989cec01c
= <jangrzegrzolka@gmail.com>
2020-09-26 00:18:40 +0200

    initial commit

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<string>> Get(int id)
+        {
+            var value = await _context.Values.SingleOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
+        }
+    }
+}
---
+++ Persistence/Migrations/20200924212115_SeedValues.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class SeedValues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Values",
+                table: "Values");
+
+            migrationBuilder.RenameTable(
+                name: "Values",
+                newName: "Values");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Values",
+                table: "Values",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Values",
+                table: "Values");
+
+            migrationBuilder.RenameTable(
+                name: "Values",
+                newName: "Values");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Values",
+                table: "Values",
+                column: "Id");
+        }
+    }
+}
